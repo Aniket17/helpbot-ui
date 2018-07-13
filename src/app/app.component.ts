bf312c4c424904b28aa46d2fdf7f0307888a6c71
Aniket Dandawate <Aniket.Dandawate@nice.com>
2018-07-13 17:24:25 +0530

    scroll down on new messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,11 @@ export class AppComponent {
   isOpen = true;
   message = "";
   messages = [
-  {
-    text: "Hi! I am Mr.Helpbot!",
-    dateSent: new Date().toString(),
-    type: "recieve"
-  }];
+    {
+      text: "Hi! I am Mr.Helpbot!",
+      dateSent: (new Date()).toDateString(),
+      type: "recieve"
+    }];
   isLoading = false;
   http;
   constructor(private client: HttpClient) {
@@ -27,10 +27,10 @@ export class AppComponent {
   }
 
   public sendMessage = (message) => {
-    if(!message){return;}
+    if (!message) { return; }
     this.isLoading = true;
     let self = this;
-    self.processMessage(message,"sent", false);
+    self.processMessage(message, "sent", false);
     this.http.post('http://localhost:5004/chat/', { sender: "", message: this.message })
       .subscribe(data => {
         self.message = "";
@@ -38,21 +38,22 @@ export class AppComponent {
         //data is array of fukcing strings and json
         data.forEach(msg => {
           try {
-            self.processMessage(msg,"recieve", true);
+            self.processMessage(msg, "recieve", true);
           } catch (error) {
             //continue.. dont bother.. its normal message
-            self.processMessage(msg,"recieve",false);
+            self.processMessage(msg, "recieve", false);
           }
         });
       });
   }
-  public processMessage = (msg, type,isJson) => {
+  public processMessage = (msg, type, isJson) => {
     let obj = {
-      text: isJson ? "<ol>"+ JSON.parse(msg) +"</ol>" : msg,
+      text: isJson ? "<ol>" + JSON.parse(msg) + "</ol>" : msg,
       type: type,
-      dateSent: (new Date()).toString()
+      dateSent: (new Date()).toDateString()
     }
     this.messages.push(obj);
+    setTimeout(()=>$('.msg_container_base')[0].scrollTop = $('.msg_container_base')[0].scrollHeight,200)
   }
   public keyDownFunction = (event) => {
     if (event.keyCode == 13) {
